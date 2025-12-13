a8e73722d064f5dd64faf98e7dc2d361599c9e3e
sunpark333 <99256042+sunpark333@users.noreply.github.com>
2025-12-13 19:54:03 +0530

    Add files via upload

---
+++ devgagan/__main__.py
@@ -0,0 +1,55 @@
+# ---------------------------------------------------
+# File Name: __main__.py
+# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
+#              and uploading them back to Telegram.
+# Author: Perry
+# Created: 2025-01-11
+# Last Modified: 2025-01-11
+# Version: 2.0.5
+# License: MIT License
+# ---------------------------------------------------
+
+import asyncio
+import importlib
+import gc
+from pyrogram import idle
+from devgagan.modules import ALL_MODULES
+from devgagan.core.mongo.plans_db import check_and_remove_expired_users
+
+# ----------------------------Bot-Start---------------------------- #
+
+loop = asyncio.get_event_loop()
+
+# Function to schedule expiry check
+async def schedule_expiry_check():
+    while True:
+        await check_and_remove_expired_users()
+        await asyncio.sleep(3600)  # Check every hour
+        gc.collect()
+
+async def devggn_boot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("devgagan.modules." + all_module)
+    print("""
+---------------------------------------------------
+📂 Bot Deployed successfully ...
+📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
+                and uploading them back to Telegram.
+👨‍💻 Author: Perry
+🗓️ Created: 2025-01-11
+🔄 Last Modified: 2025-01-11
+🛠️ Version: 2.0.5
+📜 License: MIT License
+---------------------------------------------------
+""")
+
+    asyncio.create_task(schedule_expiry_check())
+    print("Auto removal started ...")
+    await idle()
+    print("Bot stopped...")
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(devggn_boot())
+
+# ------------------------------------------------------------------ #
