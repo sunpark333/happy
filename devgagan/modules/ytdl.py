55851bc20e93cc168bfb7c8907a662e1dc12d1cd
sunpark333 <99256042+sunpark333@users.noreply.github.com>
2026-01-13 11:38:04 +0530

    Update ytdl.py

--- devgagan/modules/ytdl.py
+++ devgagan/modules/ytdl.py
@@ -2,7 +2,7 @@
 # File Name: ytdl.py (pure code)
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
-# Author: Pushpa Bhau
+# Author: Elexyz
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
 # Version: 2.0.5
@@ -118,8 +118,8 @@ async def process_audio(client, event, url, cookies_env_var=None):
                 except Exception:
                     pass
                 audio_file.tags["TIT2"] = TIT2(encoding=3, text=title)
-                audio_file.tags["TPE1"] = TPE1(encoding=3, text="Pushpa Bhau")
-                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by Pushpa Bhau")
+                audio_file.tags["TPE1"] = TPE1(encoding=3, text="Elexyz")
+                audio_file.tags["COMM"] = COMM(encoding=3, lang="eng", desc="Comment", text="Processed by Elexyz")
  
                 thumbnail_url = info_dict.get('thumbnail')
                 if thumbnail_url:
@@ -147,7 +147,7 @@ async def process_audio(client, event, url, cookies_env_var=None):
                 name=None,
                 progress_bar_function=lambda done, total: progress_callback(done, total, chat_id)
             )
-            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by Pushpa Bhau__**")
+            await client.send_file(chat_id, uploaded, caption=f"**{title}**\n\n**__Powered by Elexyz__**")
             if prog:
                 await prog.delete()
         else:
@@ -301,7 +301,7 @@ def progress_callback(done, total, user_id):
         f"│ **__Speed:__** {speed_mbps:.2f} Mbps\n"
         f"│ **__Time Remaining:__** {remaining_time_min:.2f} min\n"
         f"╰──────────────────╯\n\n"
-        f"**__Powered by Pushpa Bhau__**"
+        f"**__Powered by Elexyz__**"
     )
  
      
@@ -352,7 +352,7 @@ async def process_video(client, event, url, cookies_env_var, check_duration_and_
             return
          
         await asyncio.to_thread(download_video, url, ydl_opts)
-        title = info_dict.get('title', 'Powered by Pushpa Bhau')
+        title = info_dict.get('title', 'Powered by Elexyz')
         k = video_metadata(download_path)      
         W = k['width']
         H = k['height']
